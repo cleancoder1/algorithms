f473ae3ec1dd683de443c36fbcde1d0c716068d7
Raj Parsi <rparsi@deltadentalia.com>
2016-12-09 13:09:11 -0600

    Creating api deliverable files for week3

---
+++ week3/src/main/java/BruteCollinearPoints.java
@@ -0,0 +1,16 @@
+/**
+ * Created by rparsi on 12/9/2016.
+ */
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) {
+
+    }   // finds all line segments containing 4 points
+
+    public int numberOfSegments() {
+        return 0;
+    }       // the number of line segments
+
+    public LineSegment[] segments() {
+        return null;
+    }                // the line segments
+}
---
+++ week3/src/main/java/FastCollinearPoints.java
@@ -0,0 +1,14 @@
+
+public class FastCollinearPoints {
+
+    public FastCollinearPoints() {
+    }
+
+    public int numberOfSegments() {
+        return 0;
+    }       // the number of line segments
+
+    public LineSegment[] segments() {
+        return null;
+    }                // the line segments
+}
