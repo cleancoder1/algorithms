eaaa675deb2c82b9e23cbf37614be6a5e3e38b8d
Raj Parsi <rparsi@deltadentalia.com>
2016-12-20 16:25:59 -0600

    Fixed the code , need to understand what is addLineSegments is doing.

--- week3/src/main/java/FastCollinearPoints.java
+++ week3/src/main/java/FastCollinearPoints.java
@@ -25,104 +25,46 @@ public class FastCollinearPoints {
             System.arraycopy(points, i + 1, others, i, others.length - i);
             Arrays.sort(others, p.slopeOrder());
 
-            //find three points on others with same slope and add them to segment
-            //Assumption  works only for input with no greater than 4 collinear points
-            maxChain(p, others);
-//            for (int j = 0; j < others.length - 2; j++) {
-//
-//                //instead of this assumption of no more than 4 collinear points try to find max chain
-//
-//                boolean b1 = isSlopeWIthElementsSame(p.slopeTo(others[j]), p.slopeTo(others[j + 1]));
-//                boolean b2 = isSlopeWIthElementsSame(p.slopeTo(others[j]), p.slopeTo(others[j + 2]));
-//                boolean b3 = isSlopeWIthElementsSame(p.slopeTo(others[j + 1]), p.slopeTo(others[j + 2]));
-//                boolean equalSlope = b1 && b2 && b3;
-//                if (equalSlope) {
-//
-//                    Point[] resultPoint = new Point[4];
-//                    resultPoint[0] = p;
-//                    resultPoint[1] = others[j];
-//                    resultPoint[2] = others[j + 1];
-//                    resultPoint[3] = others[j + 2];
-//
-//                    Arrays.sort(resultPoint);
-//                    LineSegment e = new LineSegment(resultPoint[0], resultPoint[3]);
-//                    if (!isAlreadyAdded(e)) {
-//
-//                        lineSegments.add(e);
-//                    }
-//
-//                }
-//
-//
-//            }
+
+            addLineSegments(p, others);
 
 
         }
     }
 
-    private void maxChain(Point p, Point[] others) {
-
-        int chainLength = 1;
-        for (int j = 0; j < others.length - 1; j++) {
-            boolean endOfChain = false;
-            Double d1 = p.slopeTo(others[j]);
-            Double d2 = p.slopeTo(others[j + 1]);
-            if (Double.compare(d1, d2) == 0) {
-                chainLength++;
-            } else {
-                endOfChain = true;
+    private void addLineSegments(Point p, Point[] others) {
+        int start = 0, end = 1;
+        while (end < others.length) {
+            boolean equalSlope = p.slopeTo(others[end]) == p.slopeTo(others[end - 1]);
+            if (equalSlope) {
+                end++;
             }
-
-            if (chainLength >= 3) {
-
-                if (endOfChain) {
-                    Point[] resultPoint = new Point[chainLength + 1];
-                    for (int i = 1; i <= chainLength; i++) {
-                        resultPoint[i] = others[j + 1 - i];
+            if (!equalSlope || end == others.length) {
+                if (end - start >= 3) {
+                    Point[] segment = new Point[end - start + 1];
+                    segment[segment.length - 1] = p;
+                    for (int k = 0; k < segment.length - 1; k++) {
+                        segment[k] = others[start + k];
                     }
-                    resultPoint[0] = p;
-                    Arrays.sort(resultPoint);
-                    LineSegment e = new LineSegment(resultPoint[0], resultPoint[chainLength]);
-                    if (!isAlreadyAdded(e)) {
-                        lineSegments.add(e);
-                    }
-                } else {
-
-                    if (j == others.length - 2) {
-                        Point[] resultPoint = new Point[chainLength + 1];
-
-                        for (int i = 1; i <= chainLength; i++) {
-                            resultPoint[i] = others[j + 2 - i];
+                    Arrays.sort(segment);
+                    LineSegment line = new LineSegment(segment[0], segment[segment.length - 1]);
+                    boolean added = false;
+                    for (LineSegment l : lineSegments) {
+                        if (l.toString().equals(line.toString())) {
+                            added = true;
+                            break;
                         }
-                        resultPoint[0] = p;
-                        Arrays.sort(resultPoint);
-                        LineSegment e = new LineSegment(resultPoint[0], resultPoint[3]);
-                        if (!isAlreadyAdded(e)) {
-                            lineSegments.add(e);
-                        }
-
                     }
-
+                    if (!added) {
+                        lineSegments.add(line);
+                    }
                 }
+                start = end;
+                end = start + 1;
             }
         }
     }
 
-    private boolean isSlopeWIthElementsSame(double d1, double d2) {
-        return Double.compare(d1, d2) == 0;
-    }
-
-
-    private boolean isAlreadyAdded(LineSegment lineSegment) {
-
-        for (LineSegment aLineSegment : lineSegments) {
-            if (aLineSegment.toString().equals(lineSegment.toString())) {
-                return true;
-            }
-
-        }
-        return false;
-    }
 
     private void checkDuplicates(Point[] points) {
 
