6a0d975dd37e1c8944cf997b0e814cd1745f9836
cleancoder1 <rajeshwar.parsi@gmail.com>
2017-01-08 21:38:54 -0600

    Week 4 APi class setup

---
+++ week4/src/main/java/Board.java
@@ -0,0 +1,47 @@
+public class Board {
+    public Board(int[][] blocks)           // construct a board from an n-by-n array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+    {
+
+    }
+
+    public int dimension() {
+        throw new IllegalArgumentException();
+
+    }               // board dimension n
+
+    public int hamming() {
+
+        throw new IllegalArgumentException();
+
+    }               // number of blocks out of place
+
+    public int manhattan() {
+        throw new IllegalArgumentException();
+    }             // sum of Manhattan distances between blocks and goal
+
+    public boolean isGoal() {
+        throw new IllegalArgumentException();
+    }            // is this board the goal board?
+
+    public Board twin() {
+        throw new IllegalArgumentException();
+    }            // a board that is obtained by exchanging any pair of blocks
+
+    public boolean equals(Object y) {
+        throw new IllegalArgumentException();
+
+    }      // does this board equal y?
+
+    public Iterable<Board> neighbors() {
+        throw new IllegalArgumentException();
+    }   // all neighboring boards
+
+    public String toString() {
+        throw new IllegalArgumentException();
+    }            // string representation of this board (in the output format specified below)
+
+    public static void main(String[] args) {
+
+    }// unit tests (not graded)
+}
---
+++ week4/src/main/java/Solver.java
@@ -0,0 +1,24 @@
+
+public class Solver {
+    public Solver(Board initial) {
+
+    }        // find a solution to the initial board (using the A* algorithm)
+
+    public boolean isSolvable() {
+        throw new IllegalArgumentException();
+    }         // is the initial board solvable?
+
+    public int moves() {
+        throw new IllegalArgumentException();
+
+    }                  // min number of moves to solve initial board; -1 if unsolvable
+
+    public Iterable<Board> solution() {
+        throw new IllegalArgumentException();
+
+    }      // sequence of boards in a shortest solution; null if unsolvable
+
+    public static void main(String[] args) {
+
+    } // solve a slider puzzle (given below)
+}
